6d2cede31266220acc03fefbec416b01d4beb2d3
hollen9 <hollen.tw@gmail.com>
2024-09-19 21:55:11 +0000

    Initial commit

---
+++ content/first-arti.md
@@ -0,0 +1,9 @@
+Title: 初めての投稿
+Date: 2023-09-20
+Category: Home
+Tags: page
+Slug: homepage
+Author: Hollen9
+Summary: これは初めての投稿です。
+
+この記事はPelicanを使って生成された初めてのブログ記事です！
---
+++ pelicanconf.py
@@ -0,0 +1,38 @@
+AUTHOR = 'Hollen9'
+SITENAME = 'Hokkejyu'
+SITEURL = ""
+
+PATH = "content"
+
+TIMEZONE = 'Asia/Taipei'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+MARKUP = ('md', 'ipynb')  # Include 'md' if using Markdown
+# THEME = "themes/pelican-mediumfox"
+
+# Blogroll
+LINKS = (
+    ("Pelican", "https://getpelican.com/"),
+    ("Python.org", "https://www.python.org/"),
+    ("Jinja2", "https://palletsprojects.com/p/jinja/"),
+    ("You can modify those links in your config file", "#"),
+)
+
+# Social widget
+SOCIAL = (
+    ("You can add links in your config file", "#"),
+    ("Another social link", "#"),
+)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+# RELATIVE_URLS = True
